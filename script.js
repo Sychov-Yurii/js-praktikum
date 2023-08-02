88e1df918cded18132dd04158ffbe81a8222c8da
Sychov-Yurii <sychov.yurii@outlook.com>
2023-08-02 16:47:33 +0200

    add

--- script.js
+++ script.js
@@ -342,19 +342,37 @@
 
 // console.dir(boxEl)
 
-function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//     let message;
+//     // Change code below this line
+//   let totalPrice = (pricePerDroid*orderedQuantity) - customerCredits;
+//   let userTotal = 
+  
+//     // Change code above this line
+//     if (customerCredits > pricePerDroid) {
+//       message = "Insufficient funds!";
+  
+//     } else {
+//       message = `You ordered ${orderedQuantity} droids, you have ${totalPrice} credits left`
+//     }
+//     return message;
+//   }
+
+
+  function checkStorage(available, ordered) {
     let message;
     // Change code below this line
-  let totalPrice = (pricePerDroid*orderedQuantity) - customerCredits;
-  let userTotal = 
-  
+  if (ordered = 0) {
+    message = "There are no products in the order!"
+  };
+    else if (ordered > available) {
+      message = "Your order is too large, there are not enough items in stock!"
+    };
+    else {
+      message = "The order is accepted, our manager will contact you"
+    };
     // Change code above this line
-    if (customerCredits > pricePerDroid) {
-      message = "Insufficient funds!";
-  
-    } else {
-      message = `You ordered ${orderedQuantity} droids, you have ${totalPrice} credits left`
-    }
     return message;
   }
+  
 
