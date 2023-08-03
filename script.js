55c719b6280f286fe0b72d2df523d8fd06b5ac0e
Sychov-Yurii <sychov.yurii@outlook.com>
2023-08-03 20:18:15 +0200

    03 08

--- script.js
+++ script.js
@@ -381,6 +381,35 @@
 
 
 
-const isComing = confirm("Please confirm hotel reservation");
-console.log(isComing);
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log(isComing);
 
+function getShippingCost(country) {
+    let message;
+    // Change code below this line
+  switch (country) {
+  
+    case China :
+      message = `Shipping to ${country} will cost 100 credits`;
+      break;
+  
+    case Chile :
+      message = `Shipping to ${country} will cost 250 credits`;
+      break;
+  
+    case Australia :
+      message = `Shipping to ${country} will cost 170 credits`; 
+      break;
+      
+    case Jamaica :
+      message = `Shipping to ${country} will cost 120 credits`;
+      break;
+      
+      default : 
+      message = "Sorry, there is no delivery to your country"
+  
+      
+  }
+    // Change code above this line
+    return message;
+  }
