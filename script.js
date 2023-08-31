b405f19c2fc0f3aafac036880931422525a0a18c
Sychov-Yurii <sychov.yurii@outlook.com>
2023-08-31 12:24:34 +0200

    update

--- script.js
+++ script.js
@@ -384,36 +384,36 @@
 // const isComing = confirm("Please confirm hotel reservation");
 // console.log(isComing);
 
-function getShippingCost(country) {
-    let message;
-    // Change code below this line
-  switch (country) {
+// function getShippingCost(country) {
+//     let message;
+//     // Change code below this line
+//   switch (country) {
   
-    case China :
-      message = `Shipping to ${country} will cost 100 credits`;
-      break;
+//     case China :
+//       message = `Shipping to ${country} will cost 100 credits`;
+//       break;
   
-    case Chile :
-      message = `Shipping to ${country} will cost 250 credits`;
-      break;
+//     case Chile :
+//       message = `Shipping to ${country} will cost 250 credits`;
+//       break;
   
-    case Australia :
-      message = `Shipping to ${country} will cost 170 credits`; 
-      break;
+//     case Australia :
+//       message = `Shipping to ${country} will cost 170 credits`; 
+//       break;
       
-    case Jamaica :
-      message = `Shipping to ${country} will cost 120 credits`;
-      break;
+//     case Jamaica :
+//       message = `Shipping to ${country} will cost 120 credits`;
+//       break;
       
-      default : 
-      message = "Sorry, there is no delivery to your country"
+//       default : 
+//       message = "Sorry, there is no delivery to your country"
   
       
-  }
-    // Change code above this line
-    return message;
+//   }
+//     // Change code above this line
+//     return message;
 
-  }
+//   }
   
 
 
@@ -421,3 +421,7 @@ function getShippingCost(country) {
 //     delete x;
 //     return x;
 //   })(100)
+
+const mySet = new Set([1, 2, 1, 4, 3, 2]);
+const result = [...mySet];
+console.log(result);
